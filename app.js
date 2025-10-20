3798d51320feca7a6325ee034c0346087a1030d7
dongseop <vital121@hanmail.net>
2025-10-20 16:05:28 +0900

    add get tasks

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import tasks from './mock.js';
 const PORT = 3000;
 const app = express();
 
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hello Express');
 });
 
+app.get('/tasks', (req, res) => {
+  const sort = req.query.sort;
+  const count = Number(req.query.count);
+
+  const compareFn = sort === 'oldest' ? (a, b) => a.createdAt - b.createdAt : (a, b) => b.createdAt - a.createdAt;
+  let newTasks = tasks.sort(compareFn);
+  if (count) {
+    newTasks = newTasks.slice(0, count);
+  }
+  res.send(tasks);
+});
 app.listen(PORT, (err) => {
   console.log(`Server Started on ${PORT}`);
   console.log(err);
